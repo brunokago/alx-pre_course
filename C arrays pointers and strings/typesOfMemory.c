a2f1087d50c82aef590da29bada32e6d1a0463fd
brunokago <brunokago@gmail.com>
2022-06-14 09:26:21 +0300

    first commit

---
+++ C arrays pointers and strings/typesOfMemory.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int n;
+     printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
+     printf("size of type int %lu bytes\n", sizeof(int));
+
+}
